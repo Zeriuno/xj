45e81385721ff2421e414b95c1531cc0554b5f19
Zeriuno <d.pitrolo@gmx.com>
2016-01-11 21:24:25 +0100

    Début - Séance d'introduction

---
+++ MaPremiereClasseDeCeCours.java
@@ -0,0 +1,18 @@
+/*
+ Exercice introductif de M. Boyer, tous droits réservés.
+ */
+
+import java.util.Scanner;
+
+
+public class MaPremiereClasseDeCeCours {
+	public static void main(String[] args){
+		System.out.println("Hello World !");
+		int i=0;
+		boolean a = i<3;
+		char t = 'z';
+		Scanner scan = new Scanner(System.in);
+		i = scan.nextInt();
+	}
+
+}
