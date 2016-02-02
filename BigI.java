19cbf04b94236ee8bd66cdac675959900df674f2
Zeriuno <d.pitrolo@gmx.com>
2016-02-03 00:47:06 +0100

    Commentaires

--- BigI.java
+++ BigI.java
@@ -5,7 +5,7 @@ public class BigI {
 	/**
 	 * @param args
 	 */
-	
+
 	/*TP 2 - I facto*/
 	public static int factorielle(int gris){
 		if(gris < 0){
@@ -24,7 +24,7 @@ public class BigI {
 		a.add(c) ;
 		return a ;
 	}
-	
+
 	/*TP 2 - II - 2*/
 	public static boolean plusPetitQue(BigInteger fa, BigInteger fe){
 		if(fa.compareTo(fe)==1){ //la méthode retourne -1 si le premier est plus petit que le paramètre, 0 en cas d'égalité, 1 si le premier est le majeur
@@ -33,23 +33,23 @@ public class BigI {
 		else
 			return false;
 	}
-	
+
 	/*TP 2 - II - 3*/
 	public static BigInteger incr(BigInteger gr){
-		BigInteger is = BigInteger.valueOf(1); //
-		gr.add(is);
-		return gr;
+		BigInteger is = BigInteger.valueOf(1); //BigInteger est différent de int (1 sera automatiquement un int), et donc on ne peut pas les additionner directement. Il faut donc transformer 1 en BigInteger. Ici on déclare le BigInteger au nom is et il vaut 1, qui est converti en BigInteger par valueOf.
+		gr.add(is); //on ajoute un BigInteger à l'autre (1).
+		return gr; //la valeur est stockée dans le premier des deux, que l'on retourne ici.
 	}
-	
+
 	public static void main(String[] args) {
 /*		System.out.println("De quel nombre faire la factorielle ?");
 		Scanner rentre=new Scanner(System.in)             ;
 		int z = rentre.nextInt()                     ;
 		rentre.close();
-		
+
 		System.out.println(z+"!="+factorielle(z));*/
-		
-		
+
+
 		BigInteger ra = new BigInteger("468354");
 		BigInteger pl = new BigInteger("58794");
 		BigInteger apla = new BigInteger("59874");
@@ -58,9 +58,9 @@ public class BigI {
 		System.out.println("BigInteger augmenté de 1 : " + ra);
 		bbb(ra, pl, apla);
 		System.out.println("Somme des trois BigInteger : " + ra);
-		
+
 		System.out.println(" ");
-				
+
 	}
 
 }
