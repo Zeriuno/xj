8208261aa5d43c047737dee3922537a231bc19dd
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-03-17 22:51:39 +0100

    Mois

---
+++ 3-17-1-Mois.java
@@ -0,0 +1,22 @@
+
+public class Mois {
+
+	int numero ;
+	int jours  ;
+	
+	//costructeurs
+	public Mois ()
+	{
+	}
+	
+	public Mois(int numero)
+	{
+		this()               ;
+		this.numero = numero ;
+	}
+	public Mois (int numero, int jours)
+	{
+		this(numero)       ;
+		this.jours = jours ;
+	}
+}
