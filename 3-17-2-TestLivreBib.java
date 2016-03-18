d31aa9211674c804844af445b6157632f1a3f4a4
Zeriuno <d.pitrolo@gmx.com>
2016-03-18 01:40:54 +0100

    Plus de tests -debug

--- 3-17-2-TestLivreBib.java
+++ 3-17-2-TestLivreBib.java
@@ -1,9 +1,8 @@
-
 public class TestLivreBib {
 
 	public static void main(String[] args) {
 		Livre Q = new Livre("Q", "Luther Blisset", 2002);
-		
+
 		System.out.println(Q)             ;
 		System.out.println(Q.getTitre())  ;
 		System.out.println(Q.getAuteur()) ;
@@ -11,7 +10,7 @@ public class TestLivreBib {
 		System.out.println(Q.getNbrPage());
 		Q.setNbrPage(450);
 		System.out.println(Q.getNbrPage());
-		
+
 		Livre Altai = new Livre("Altai", "Wu Ming", 2009, 398);
 		System.out.println(Altai);
 		System.out.println(Altai.getTitre());
@@ -20,14 +19,17 @@ public class TestLivreBib {
 		System.out.println(Altai.getNbrPage());
 		Altai.setNbrPage(0);
 		System.out.println(Altai.getNbrPage());
-		
+
 		Bibliotheque petite = new Bibliotheque(2);
 		Bibliotheque grande = new Bibliotheque();
-		
+
 		System.out.println("Ajout de Q dans la bibliothèque \"petite\": \n" + petite.ajout(Q))    ;
 		System.out.println("Ajout de Altai dans la bibliothèque \"petite\": \n" + petite.ajout(Altai));
-		
-		
+		System.out.println(petite.estPresent(Q));
+		petite.retire(Q);
+		System.out.println(petite.estPresent(Q));
+
+
 	}
 
 }
