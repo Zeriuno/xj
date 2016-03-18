18f523fb3a3d5dab8ccb9fec4f6d45e9c73f4893
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-03-18 23:49:00 +0100

    maj

--- 3-17-1-Mois.java
+++ 3-17-1-Mois.java
@@ -19,4 +19,55 @@ public class Mois {
 		this(numero)       ;
 		this.jours = jours ;
 	}
+	
+	//ajout de numero
+	
+	public void setNumber(int numero)
+	{
+		if(this.numero == 0)
+		{
+		    this.numero = numero;	
+		}
+		else
+			System.out.println("Erreur, numéro déjà renseigné");
+	}
+	
+	//ajout du nombre de jours
+	
+	public void setDays(int jours)
+	{
+		if(this.jours == 0)
+		{
+			if((0 < jours) && (jours < 32))
+			{
+				this.jours = jours;
+			}
+			else 
+				System.out.println("Erreur, la valeur \"jours\" fournie fausse.");
+		}
+		else
+			System.out.println("Erreur, la variable \"jours\" du Mois est déjà affectée");
+	}
+	
+	//getters
+	
+	public int getJours()
+	{
+		return this.jours;
+	}
+	
+	public int getNumero()
+	{
+		return this.numero;
+	}
+	
+	public String toString()
+	{
+		StringBuilder s = new StringBuilder();
+		s.append("Numero d'ordre du mois : ");
+		s.append(this.numero);
+		s.append("\nNumero de jours : ");
+		s.append(jours);
+		return s.toString();
+	}
 }
