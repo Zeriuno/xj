bb2f11fce7293bd235d6a30e1ca189cbf16310d7
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-06-11 16:46:15 +0200

    Fenêtre avec fond coloré

---
+++ 6-26-3-fenetre.java
@@ -0,0 +1,33 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Fenetre extends JFrame
+{
+	public Fenetre()
+	{
+		//dimensions
+		this.setSize(500, 100);
+		//fermeture
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//position, centre
+		this.setLocationRelativeTo(null);
+		//titre
+		this.setTitle("Fenêtre");
+		//visible
+		this.setVisible(true);
+		
+		//Un JPanel
+		JPanel pan = new JPanel();
+		
+		//Couleur de fond de JPanel
+		pan.setBackground(Color.CYAN);
+		
+		//On met pan dans la fenêtre
+		this.setContentPane(pan);
+		
+	}
+
+}
