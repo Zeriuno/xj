6edc6f08daf9fee99a5b3b50608ce430624f783a
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-01 19:16:56 +0200

    Vieil oubli

--- 4-25-3-Enseignant.java
+++ 4-25-3-Enseignant.java
@@ -1,24 +1,32 @@
 /*
- * 
- * 
+ *
+ *
  * Universite ArrayList<Personne> personnel
  * Personne (String prenom, String nom)
  * Etudiant (int numEtudiant)
  * Enseignant (String matiere)
- * 
+ *
  * méthodes: ajout, retire, contient, toString
- * 
+ *
  * On peut ranger dans le même ArrayList des Personnes,
  * Etudiants, Enseignants
  */
 
-public class Enseignant extends Personne
+public class Enseignant extends Personne implements Comparable<Enseignant>
 {
 	String matiere ;
-	
+
 	public Enseignant(String prenom, String nom, String matiere)
 	{
 		super(prenom, nom) ;
 		this.matiere = matiere ;
+	}
+
+	public int compareTo(Enseignant arg0)
+	{
+		/*
+		selon la taille de l'octet:
+		0, égalité
+		*/
 	}
 }
