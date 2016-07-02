41c7022549757dde2e15e86a0ea6443102e82894
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-02 14:16:30 +0200

    En cours

--- 6-21-5-suitedomino.java
+++ 6-21-5-suitedomino.java
@@ -1,12 +1,28 @@
 import java.util.ArrayList;
 
-
 public class SuiteDomino {
 	
 	private ArrayList<Domino> suite=new ArrayList<Domino>();
-	
-	public SuiteDomino() {
-		// TODO Auto-generated constructor stub
+		
+	public boolean ajoutTesselleDebut(Domino tesselle){
+		if(suite.isEmpty())
+		{
+			suite.add(tesselle) ;
+			return true         ;
+		}
+		else
+		{
+			if(tesselle.compatible(suite.get(0)))
+			{
+				
+			}
+			else
+			{
+				if
+			}
+		}
 	}
+	
+//	public void 
 
 }
