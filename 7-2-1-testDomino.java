14e00d14808f05ca264f509b0a76ce51451dca10
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-02 21:32:33 +0200

    Test des dominos - Création des blocs

---
+++ 7-2-1-testDomino.java
@@ -0,0 +1,14 @@
+public class Main {
+	
+	public static void main(String[] args)
+	{
+		Domino a = new Domino();
+		System.out.println(a);
+		Domino b = new Domino(1, true);
+		System.out.println(b);
+		Domino c = new Domino(1,6);
+		System.out.println(c);
+		Domino d = new Domino(4,6);
+		System.out.println(d);
+	}
+}
